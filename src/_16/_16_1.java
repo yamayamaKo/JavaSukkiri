3b433eb39aa7e12f03f9f2dcc9ab63ae16fe19bc
yamayamaKo <yamachanwow@gmail.com>
2023-04-26 23:37:29 +0900

    finish chapter16

---
+++ src/_16/_16_1.java
@@ -0,0 +1,16 @@
+package _16;
+
+import java.util.ArrayList;
+
+public class _16_1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> points = new ArrayList<Integer>();
+        points.add(10);
+        points.add(5);
+        points.add(100);
+        points.add(86);
+        for (int i : points) {
+            System.out.println(i);
+        }
+    }
+}
